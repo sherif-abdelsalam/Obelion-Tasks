f069a0b91f172e3c87b8a3768b098a63e70f7bcd
Sherif Hodhod <sherif.abdelsalam.111@gmail.com>
2024-11-27 22:23:07 +0200

    counter enhanced

--- Login Page/script.js
+++ Login Page/script.js
@@ -1,16 +1,26 @@
 const counterDiv = document.querySelector('.js-counter');
 counterDiv.innerHTML = 30;
 
+// function counterDown(){
+//     let counter = 30;
+//     for (let i = 1; i <= counter; i++) {
+
+//         setTimeout(() => {
+//             counter--;
+//             counterDiv.innerHTML = counter;
+    
+//         }, i*1000);
+//     }
+// }
+
 function counterDown(){
     let counter = 30;
-    for (let i = 1; i <= counter; i++) {
-
-        setTimeout(() => {
-            counter--;
-            counterDiv.innerHTML = counter;
-    
-        }, i*1000);
-    }
+    let interval = setInterval(() => {
+        counter--;
+        counterDiv.innerHTML = counter;
+        if(counter == 0){
+            clearInterval(interval);
+        }
+    }, 1000);
 }
-
 counterDown();
